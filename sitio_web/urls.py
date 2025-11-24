2240dfae3f5634ea144b9335c1618f7a6b870809
Dariana Peña <darianaperez616@gmail.com>
2025-11-23 20:21:49 -0600

    Restore login as homepage

--- sitio_web/urls.py
+++ sitio_web/urls.py
@@ -1,7 +1,12 @@
-﻿from django.urls import path
-from django.http import HttpResponse
+﻿from django.contrib import admin
+from django.urls import path, include
+from django.shortcuts import redirect
 
-def home(request):
-    return HttpResponse('<h1>✅ SITIO REPARADO</h1><p>Despliegue exitoso</p>')
+def root_redirect(request):
+    return redirect('login')  # Redirige al login
 
-urlpatterns = [path('', home)]
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', root_redirect),
+    path('usuarios/', include('usuarios.urls')),
+]
