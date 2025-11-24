8adcc9c79f8e14c43d55a9be5d8f80681c10e44a
Dariana Peña <darianaperez616@gmail.com>
2025-11-23 22:44:39 -0600

    FIX: Complete URL patterns

--- usuarios/urls.py
+++ usuarios/urls.py
@@ -2,8 +2,10 @@
 from . import views
 
 urlpatterns = [
+    path('', views.login_view, name='login'),
     path('login/', views.login_view, name='login'),
     path('registro/', views.registro_view, name='registro'),
     path('acceso_base_datos/', views.acceso_base_datos, name='acceso_base_datos'),
-    path('', views.index_view, name='index'),
+    path('procesar_registro/', views.procesar_registro, name='procesar_registro'),
+    path('procesar_login/', views.procesar_login, name='procesar_login'),
 ]
