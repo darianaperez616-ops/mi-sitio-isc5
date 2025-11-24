d2c714948b66a5c824248088326863a576026cf2
Dariana Peña <darianaperez616@gmail.com>
2025-11-23 21:42:56 -0600

    FIX: Add acceso_base_datos URL route

--- usuarios/urls.py
+++ usuarios/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path('login/', views.login_view, name='login'),
     path('registro/', views.registro_view, name='registro'),
+    path('acceso_base_datos/', views.acceso_base_datos, name='acceso_base_datos'),
     path('', views.index_view, name='index'),
 ]
