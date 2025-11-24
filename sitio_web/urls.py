3a9efab79ad31e6414dc782a395a85e0e2db5aad
Dariana Peña <darianaperez616@gmail.com>
2025-11-23 19:01:55 -0600

    Fix URL conflicts and add basic views

--- sitio_web/urls.py
+++ sitio_web/urls.py
@@ -1,14 +1,9 @@
-from django.contrib import admin
+﻿from django.contrib import admin
 from django.urls import path, include
-from django.shortcuts import redirect
 from usuarios.views import index_view
 
-def root(request):
-    return redirect('login')  # o a 'index', según quieras
-
 urlpatterns = [
-    path('', root),                  # ruta principal
-    path('index/', index_view, name='index'),  # para acceder directamente a index.html
-    path('', include('usuarios.urls')),   # rutas de la app usuarios
     path('admin/', admin.site.urls),
+    path('', index_view, name='index'),
+    path('usuarios/', include('usuarios.urls')),
 ]
