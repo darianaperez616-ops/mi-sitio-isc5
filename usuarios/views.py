51ab8df54607c92266e513f2cd893ad7b188e920
Dariana Peña <darianaperez616@gmail.com>
2025-11-23 21:09:12 -0600

    ADD: All missing views for templates

--- usuarios/views.py
+++ usuarios/views.py
@@ -5,7 +5,23 @@ def login_view(request):
     return render(request, 'usuarios/login.html')
 
 def registro_view(request):
-    return HttpResponse('<h1>Página de registro</h1><a href="/">Volver al login</a>')
+    return render(request, 'usuarios/registro.html')
+
+def acceso_base_datos(request):
+    return render(request, 'usuarios/acceso_base_datos.html')
+
+def home_view(request):
+    return render(request, 'usuarios/home.html')
 
 def index_view(request):
-    return HttpResponse('INDEX')
+    return render(request, 'usuarios/index.html')
+
+# Agrega aquí cualquier otra vista que necesites
+def opciones_view(request):
+    return render(request, 'usuarios/opciones.html')
+
+def configuracion_view(request):
+    return render(request, 'usuarios/configuracion.html')
+
+def creditos_view(request):
+    return render(request, 'usuarios/creditos.html')
