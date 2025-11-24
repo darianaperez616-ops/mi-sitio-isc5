9881e35f16c08f012b12150b7f4685bb2201905b
Dariana Peña <darianaperez616@gmail.com>
2025-11-23 20:27:39 -0600

    FIX: Add missing login_view function

--- usuarios/views.py
+++ usuarios/views.py
@@ -1,6 +1,9 @@
 ﻿from django.shortcuts import render
 from django.http import HttpResponse
 
+def login_view(request):
+    return render(request, 'usuarios/login.html')
+
 def index_view(request):
     return HttpResponse('<h1>¡Mi sitio funciona! 🎉</h1><p>Bienvenido al sitio</p>')
 
