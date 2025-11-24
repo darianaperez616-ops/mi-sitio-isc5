2241fc10ea56fe5acbd95c99393f44569ff390eb
Dariana Peña <darianaperez616@gmail.com>
2025-11-23 21:00:55 -0600

    ADD: Basic registro_view

--- usuarios/views.py
+++ usuarios/views.py
@@ -4,8 +4,8 @@ from django.http import HttpResponse
 def login_view(request):
     return render(request, 'usuarios/login.html')
 
+def registro_view(request):
+    return HttpResponse('<h1>Página de registro</h1><p>Próximamente...</p>')
+
 def index_view(request):
     return HttpResponse('INDEX')
-    
-def registro_view(request):
-    return HttpResponse('REGISTRO')
