627b66087ba3af8ff6f73bca236891bde28e40a4
Dariana Peña <darianaperez616@gmail.com>
2025-11-23 22:37:28 -0600

    FIX: Add missing registro_view function

--- usuarios/views.py
+++ usuarios/views.py
@@ -2,7 +2,13 @@
 from django.http import HttpResponse
 
 def login_view(request):
-    # Prueba simple - sin lógica compleja
     return render(request, 'usuarios/login.html')
 
-# ... (mantén las otras vistas)
+def registro_view(request):
+    return render(request, 'usuarios/registro.html')
+
+def acceso_base_datos(request):
+    return HttpResponse('<h1>Base de Datos</h1><a href=\"/\">← Volver</a>')
+
+def index_view(request):
+    return HttpResponse('INDEX')
